c1e39a766e177c2ef2ece509cda01c34b68d0847
meckenbach <mle9@student.london.ac.uk>
2021-06-04 14:08:20 +0200

    ex 10.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Formik } from 'formik';
 
-import Text from './Text';
+import SignInForm from './SignInForm';
+
+const initialValues = {
+  username: "",
+  password: "",
+};
+
+const onSubmit = (values) => {
+  console.log(values);
+};
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      component={({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    />
+  );
 };
 
 export default SignIn;
