ccda9429baf519d80574aa54d1b253cd3815120d
meckenbach <mle9@student.london.ac.uk>
2021-06-04 11:26:48 +0200

    ex 10.5

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,16 +1,65 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import Text from './Text';
+import Chip from './Chip';
+import Statistic from './Statistic';
 
-const RepositoryItem = ({ fullName, description, language, stargazersCount, forksCount, ratingAverage }) => {
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: "3%",
+    flexDirection: "column",
+    height: 170,
+    justifyContent: "space-between",
+  },
+  header: {
+    flexDirection: "row",
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  main: {
+    flexDirection: "column",
+    justifyContent: "space-between",
+    marginLeft: 10,
+    height: 90,
+    width: 250,
+  },
+  thumbnail: {
+    width: 50,
+    height: 50,
+    borderRadius: 6,
+  },
+});
+
+const kilofy = (count) => {
+  return count >= 1000
+    ? (count / 1000).toFixed(1) + 'k'
+    : count;
+};
+
+const RepositoryItem = ({ fullName, description, language, stargazersCount, forksCount, reviewCount, ratingAverage, ownerAvatarUrl }) => {
   return (
-    <View>
-      <Text>Full name: {fullName}</Text>
-      <Text>Description: {description}</Text>
-      <Text>Language: {language}</Text>
-      <Text>Stars: {stargazersCount}</Text>
-      <Text>Forks: {forksCount}</Text>
-      <Text>Rating: {ratingAverage}</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image 
+          style={styles.thumbnail}
+          source={{
+            uri: ownerAvatarUrl,
+          }} />
+        <View style={styles.main}>
+          <Text fontSize='subheading' fontWeight='bold'>{fullName}</Text>
+          <Text color='textSecondary'>{description}</Text>
+          <Chip text={language} />
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <Statistic label="Stars">{kilofy(stargazersCount)}</Statistic>
+        <Statistic label="Forks">{kilofy(forksCount)}</Statistic>
+        <Statistic label="Review">{kilofy(reviewCount)}</Statistic>
+        <Statistic label="Rating">{kilofy(ratingAverage)}</Statistic>
+      </View>
     </View>
   );
 };
